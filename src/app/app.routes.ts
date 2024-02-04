c3d51375e48cf4c8e5b8168de197016d970354cb
NEOCEL Olivier <oneocel@cross-systems.com>
2024-02-04 14:46:05 +0100

    Step v1 complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,34 @@
 import { Routes } from '@angular/router';
 import {ModelAndColorComponent} from "./components/model-and-color/model-and-color.component";
+import {LocalStorageService} from "./services/local-storage.service";
+import {inject} from "@angular/core";
 
+const ConfigGuard = (): boolean => {
+  const localStorageService: LocalStorageService = inject(LocalStorageService);
+  return localStorageService.getItem('selectedModel') !== null
+    && localStorageService.getItem('selectedColor') !== null;
+}
+
+const SummaryGuard = (): boolean => {
+  return false;
+}
 export const routes: Routes = [
   { path: '', component: ModelAndColorComponent },
   {
     path: 'model',
-    loadComponent: () => import('./components/model-and-color/model-and-color.component').then((m) => m.ModelAndColorComponent),
+    loadComponent: () => import('./components/model-and-color/model-and-color.component')
+      .then((m) => m.ModelAndColorComponent),
   },
   {
     path: 'config',
-    loadComponent: () => import('./components/config-and-options/config-and-options.component').then((m) => m.ConfigAndOptionsComponent),
+    loadComponent: () => import('./components/config-and-options/config-and-options.component')
+      .then((m) => m.ConfigAndOptionsComponent),
+    canActivate: [ConfigGuard]
   },
   {
     path: 'summary',
-    loadComponent: () => import('./components/summary/summary.component').then((m) => m.SummaryComponent),
+    loadComponent: () => import('./components/summary/summary.component')
+      .then((m) => m.SummaryComponent),
+    canActivate:[SummaryGuard]
   }
 ];
--- src/app/components/model-and-color/model-and-color.component.ts
+++ src/app/components/model-and-color/model-and-color.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Model} from "../../api/model";
-import {CurrencyPipe, NgForOf} from "@angular/common";
+import {CurrencyPipe, NgForOf, NgIf} from "@angular/common";
 import {ModelService} from "../../services/model.service";
 import { Color } from '../../api/color';
 import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
-import {filter, from, map, Observable, of, tap} from "rxjs";
+import {filter, from, map, Observable, of, Subject, takeUntil, tap} from "rxjs";
+import {LocalStorageService} from "../../services/local-storage.service";
 
 @Component({
   selector: 'app-model-and-color',
@@ -13,11 +14,12 @@ import {filter, from, map, Observable, of, tap} from "rxjs";
     CurrencyPipe,
     NgForOf,
     ReactiveFormsModule,
+    NgIf,
   ],
   templateUrl: './model-and-color.component.html',
   styleUrl: './model-and-color.component.scss'
 })
-export class ModelAndColorComponent implements OnInit {
+export class ModelAndColorComponent implements OnInit, OnDestroy {
   formGroup: FormGroup;
   models: Model[] = [];
   colors: Color[] = [];
@@ -25,7 +27,11 @@ export class ModelAndColorComponent implements OnInit {
   selectedModel?: Model;
   selectedColor?: Color;
 
-  constructor(private modelService: ModelService, private formBuilder: FormBuilder) {
+  private destroySubject: Subject<void> = new Subject();
+
+  constructor(private modelService: ModelService,
+              private localStorageService: LocalStorageService,
+              private formBuilder: FormBuilder) {
     this.formGroup = this.formBuilder.group({
       modelControl: [''], // Initialize with an empty value
       colorControl: ['']
@@ -38,25 +44,60 @@ export class ModelAndColorComponent implements OnInit {
     this.formGroup
       .get('modelControl')
       ?.valueChanges.pipe(
+      takeUntil(this.destroySubject),
       tap(() => {
+        this.setSelectedModel();
         this.formGroup.patchValue({colorControl: ''});
       })
     ).subscribe();
   }
 
-  onSelect(event: Event){
+  ngOnDestroy(): void {
+    this.destroySubject.next();
+  }
+
+  onModelSelect(event: Event){
     const { target } = event;
     if (target) {
       const modelCode = (target as HTMLSelectElement).value;
       from(this.models).pipe(
+        takeUntil(this.destroySubject),
         filter(model => model.code === modelCode),
+        tap(model => this.localStorageService.setItem('selectedModel', JSON.stringify(model))),
         map(model => model.colors)
-      ).subscribe( colors => this.colors = colors);
+      ).subscribe( colors => {
+        this.colors = colors;
+        this.selectedColor = colors[0];
+        this.formGroup.patchValue({colorControl: this.selectedColor.code});
+        this.localStorageService.setItem('selectedColor', JSON.stringify(this.selectedColor));
+      });
     }
+  }
 
+  onColorSelect(event: Event){
+    const { target } = event;
+    if (target) {
+      const colorCode = (target as HTMLSelectElement).value;
+      from(this.colors).pipe(
+        takeUntil(this.destroySubject),
+        filter(color => color.code === colorCode),
+      ).subscribe( color => {
+        this.selectedColor = color;
+        this.localStorageService.setItem('selectedColor', JSON.stringify(this.selectedColor));
+      });
+    }
   }
 
   private getModels() {
     this.modelService.getModels().subscribe(models => this.models = models);
+  }
+
+  private setSelectedModel() {
+    from(this.models).pipe(
+      takeUntil(this.destroySubject),
+      filter(model => model.code === this.formGroup.get('modelControl')?.value)
+    ).subscribe(model => {
+      this.selectedModel = model;
+    });
   }
 }
